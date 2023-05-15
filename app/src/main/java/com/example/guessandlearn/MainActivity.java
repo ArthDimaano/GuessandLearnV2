98bbe81f15a6ff16bf73c4d48adeeb3ff46e5f35
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-05-15 22:01:58 +0800

    hehe

--- app/src/main/java/com/example/guessandlearn/MainActivity.java
+++ app/src/main/java/com/example/guessandlearn/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.button_first);
+        btn = (Button) findViewById(R.id.play);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
