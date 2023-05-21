7e51675ed82b7397dc3926a87773df2eb5254c29
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-05-21 23:19:30 +0800

    ching

---
+++ app/src/main/java/com/example/guessandlearn/hard.java
@@ -0,0 +1,13 @@
+package com.example.guessandlearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hard);
+    }
+}
