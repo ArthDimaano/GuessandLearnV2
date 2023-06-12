2e7b5f2049d63af6a60f923c7b1a06ffbea6e24b
ArthDimaano <131332012+ArthDimaano@users.noreply.github.com>
2023-06-13 02:13:00 +0800

    VERY NICE

--- app/src/main/java/com/example/guessandlearn/pronouns.java
+++ app/src/main/java/com/example/guessandlearn/pronouns.java
@@ -15,12 +15,57 @@ public class pronouns extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pronouns);
 
-        btn = (ImageButton) findViewById(R.id.mobo);
-        final MediaPlayer mobo = MediaPlayer.create(this, R.raw.motherboard_normal);
+        btn = (ImageButton) findViewById(R.id.java);
+        final MediaPlayer java = MediaPlayer.create(this, R.raw.java_easy);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mobo.start();
+                java.start();
+            }
+        });
+
+        btn = (ImageButton) findViewById(R.id.printer);
+        final MediaPlayer printer = MediaPlayer.create(this, R.raw.printer_easy);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                printer.start();
+            }
+        });
+
+        btn = (ImageButton) findViewById(R.id.virus);
+        final MediaPlayer virus = MediaPlayer.create(this, R.raw.virus_easy);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                virus.start();
+            }
+        });
+
+        btn = (ImageButton) findViewById(R.id.mouse);
+        final MediaPlayer mouse = MediaPlayer.create(this, R.raw.mouse_easy);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mouse.start();
+            }
+        });
+
+        btn = (ImageButton) findViewById(R.id.block);
+        final MediaPlayer block = MediaPlayer.create(this, R.raw.block_easy);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                block.start();
+            }
+        });
+
+        btn = (ImageButton) findViewById(R.id.variable);
+        final MediaPlayer variable = MediaPlayer.create(this, R.raw.variable_easy);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                variable.start();
             }
         });
 
