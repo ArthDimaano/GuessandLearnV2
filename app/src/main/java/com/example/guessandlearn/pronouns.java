5e89e9e27380f90360c6fb6a582049f21eef1ebf
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-06-12 23:52:22 +0800

    teatea

--- app/src/main/java/com/example/guessandlearn/pronouns.java
+++ app/src/main/java/com/example/guessandlearn/pronouns.java
@@ -2,13 +2,27 @@ package com.example.guessandlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class pronouns extends AppCompatActivity {
 
+    private ImageButton btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pronouns);
+
+        btn = (ImageButton) findViewById(R.id.mobo);
+        final MediaPlayer mobo = MediaPlayer.create(this, R.raw.motherboard_normal);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mobo.start();
+            }
+        });
+
     }
 }
