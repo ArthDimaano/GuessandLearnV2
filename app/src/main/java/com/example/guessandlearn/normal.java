c6dbd9ed01825739fe32efae14b416a26508eeca
ArthDimaano <131332012+ArthDimaano@users.noreply.github.com>
2023-06-12 20:50:22 +0800

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- app/src/main/java/com/example/guessandlearn/normal.java
+++ app/src/main/java/com/example/guessandlearn/normal.java
@@ -2,6 +2,9 @@ package com.example.guessandlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class normal extends AppCompatActivity {
